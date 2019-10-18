38821a7021543f2f0349e16f8550ef9363246052
shunsuke_sakurai <shunsuke_sakurai@gmail.com>
2019-10-18 12:02:07 +0900

    chore: Init

---
+++ src/components/molecules/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import sc from 'styled-components';
+
+const Footer = () => {
+  return <Footer />;
+};
+
+const _Footer = sc.div`
+    width: 100%;
+ `;
+
+export default Footer;
